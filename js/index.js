52deb2c145b68f56dd45f2fba9de450cd98529da
Gurtaran Singh <79128432+gurtaransingh@users.noreply.github.com>
2023-12-19 10:36:44 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ listenBtn.addEventListener('click', async()=>{
 
     try{
         const text = document.getElementById('text-input').value;
-        fetch('https://64d0-49-43-99-27.ngrok-free.app/synthesize', {
+        fetch('https://d38d-112-196-126-3.ngrok-free.app/synthesize', {
             method: 'POST',
             body: JSON.stringify({ text }), // Include the necessary data in the request body
             headers: {
